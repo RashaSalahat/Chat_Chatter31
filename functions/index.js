3e2b20ec87dead0872ff066350fb2c554d0173c1
RashaSalahat <rasha.salahat2000@gmail.com>
2022-05-06 14:21:39 +0300

    Send

---
+++ functions/index.js
@@ -0,0 +1,87 @@
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//   functions.logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
+// here we'll add the cloud functions
+const StreamChat = require('stream-chat').StreamChat; //authenticate the stream api , generate tokens
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+
+admin.initializeApp();
+// from env variables
+//instance to create our stream chat
+const serverClient = StreamChat.getInstance(functions.config().stream.key, functions.config().stream.secret);
+
+
+// When a user is deleted from Firebase their associated Stream account is also deleted.
+exports.deleteStreamUser = functions.auth.user().onDelete((user, context) => {
+  return serverClient.deleteUser(user.uid);
+});
+
+// Create a Stream user and return auth token.
+exports.createStreamUserAndGetToken = functions.https.onCall(async (data, context) => {
+  // Checking that the user is authenticated.
+  if (!context.auth) {
+    // Throwing an HttpsError so that the client gets the error details.
+    throw new functions.https.HttpsError('failed-precondition', 'The function must be called ' +
+      'while authenticated.');
+  } else {
+    try {
+      // Create user using the serverClient.
+      await serverClient.upsertUser({
+        id: context.auth.uid,
+        name: context.auth.token.name,
+        email: context.auth.token.email,
+        image: context.auth.token.image,
+      });
+
+      /// Create and return user auth token.
+      return serverClient.createToken(context.auth.uid);
+    } catch (err) {
+      console.error(`Unable to create user with ID ${context.auth.uid} on Stream. Error ${err}`);
+      // Throwing an HttpsError so that the client gets the error details.
+      throw new functions.https.HttpsError('aborted', "Could not create Stream user");
+    }
+  }
+});
+
+// Get Stream user token.
+exports.getStreamUserToken = functions.https.onCall((data, context) => {
+  // Checking that the user is authenticated.
+
+  if (!context.auth) {
+    // Throwing an HttpsError so that the client gets the error details.(if auth is null)
+    throw new functions.https.HttpsError('failed-precondition', 'The function must be called ' +
+      'while authenticated.');
+  } else {
+    try {
+      // we use the firebase uid as id
+      return serverClient.createToken(context.auth.uid);
+    } catch (err) {
+      console.error(`Unable to get user token with ID ${context.auth.uid} on Stream. Error ${err}`);
+      // Throwing an HttpsError so that the client gets the error details.
+      throw new functions.https.HttpsError('aborted', "Could not get Stream user");
+    }
+  }
+});
+
+// Revoke the authenticated user's Stream chat token.
+exports.revokeStreamUserToken = functions.https.onCall((data, context) => {
+  // Checking that the user is authenticated.
+  if (!context.auth) {
+    // Throwing an HttpsError so that the client gets the error details.
+    throw new functions.https.HttpsError('failed-precondition', 'The function must be called ' +
+      'while authenticated.');
+  } else {
+    try {
+      return serverClient.revokeUserToken(context.auth.uid);
+    } catch (err) {
+      console.error(`Unable to revoke user token with ID ${context.auth.uid} on Stream. Error ${err}`);
+      // Throwing an HttpsError so that the client gets the error details.
+      throw new functions.https.HttpsError('aborted', "Could not get Stream user");
+    }
+  }
+});
---
+++ lib/screens/splash_screen.dart
@@ -0,0 +1,84 @@
+import 'dart:async';
+import 'package:chatter/screens/screens.dart';
+import 'package:chatter/screens/sign_in_screen.dart';
+import 'package:cloud_functions/cloud_functions.dart';
+import 'package:firebase_auth/firebase_auth.dart' as firebase;
+import 'package:flutter/material.dart';
+import 'package:stream_chat_flutter_core/stream_chat_flutter_core.dart';
+
+import 'home_screen.dart';
+
+
+//Authentications:
+class SplashScreen extends StatefulWidget {
+  static Route get route => MaterialPageRoute(
+        builder: (context) => const SplashScreen(),
+      );
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  late final StreamSubscription<firebase.User?> listener; // this give us the user
+
+  @override
+  void initState() {
+    super.initState();
+    _handleAuthenticatedState();
+  }
+
+  Future<void> _handleAuthenticatedState() async {
+    final auth = firebase.FirebaseAuth.instance;
+    if (!mounted) {
+      return;
+    }
+    listener = auth.authStateChanges().listen((user) async {
+      if (user != null) {
+        //to know if we are authenticated
+        // get Stream user token
+        // you call the function after you got it
+        final callable =
+            FirebaseFunctions.instance.httpsCallable('getStreamUserToken');//stream token
+
+        final results = await Future.wait([
+          callable(),
+          // delay to show loading indicator
+          Future.delayed(const Duration(milliseconds: 700)),
+        ]);
+
+        // connect Stream user
+
+        final client = StreamChatCore.of(context).client;
+        await client.connectUser(
+          User(id: user.uid),
+          results[0]!.data,
+        );
+
+        // authenticated
+        Navigator.of(context).pushReplacement(HomeScreen.route);
+      } else {
+        // delay to show loading indicator
+        await Future.delayed(const Duration(milliseconds: 700));
+        // not authenticated
+        Navigator.of(context).pushReplacement(SignInScreen.route);
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    listener.cancel();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: CircularProgressIndicator(),
+      ),
+    );
+  }
+}
